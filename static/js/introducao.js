dd9bedc49aa07e421240b9548f527bf9b8a5e206
Cleyson Lima <cleysonph@gmail.com>
2022-09-28 14:20:05 -0300

    initial commit

---
+++ static/js/introducao.js
@@ -0,0 +1,86 @@
+// Comando que exibe informações no console
+// console.log("Olá Mundo!")
+
+// Variaveis e tipos de dados
+// string
+// let nome = "Cleyson Lima"
+// console.log(nome)
+
+// // number
+// let idade = 26
+// console.log(idade)
+
+// // boolean
+// let ehProgramador = true
+// console.log(ehProgramador)
+
+// // Operações matematicas
+// let num1 = 10
+// let num2 = 5
+
+// // Soma
+// console.log("Soma")
+// console.log(num1 + num2)
+
+// // Subtração
+// console.log("Subtração")
+// console.log(num1 - num2)
+
+// // Multiplicação
+// console.log("Multiplicação")
+// console.log(num1 * num2)
+
+// // Divisão
+// console.log("Divisão")
+// console.log(num1 / num2)
+
+// // Modulo
+// console.log("Modulo")
+// console.log(num1 % num2)
+
+// // Arrays
+// let notas = [10, 8.9, 7.3, 8.5]
+// console.log(notas)
+// console.log(notas[0])
+// console.log(notas[3])
+
+// notas[0] = 8.5
+// console.log(notas)
+
+// notas.push(9.1)
+// console.log(notas)
+
+// // Laços de repetição
+// let notas = [10, 8.9, 7.3, 8.5]
+
+// // For tradicional
+// for (let i = 0; i < 4; i++) {
+//   console.log(notas[i])
+// }
+// console.log("Teminou o for")
+
+// // For of
+// for (let nota of notas) {
+//   console.log(nota);
+// }
+// console.log("Terminou o for of")
+
+// // Objetos JavaScript
+// let aluno = {
+//   nome: "Cleyson Lima",
+//   idade: 26,
+//   notas: [10, 8.9, 7.3, 8.5],
+//   contato: {
+//     telefone: "(11) 99999-9999",
+//     email: "cleyson@mail.com"
+//   }
+// }
+// console.log(aluno)
+// console.log(aluno.nome)
+// console.log(aluno.notas[0])
+// console.log(aluno.contato.email)
+
+// // Manipulando o DOM
+// let elemento = document.getElementById("titulo")
+// console.log(elemento)
+// elemento.innerHTML = "Alterado via JS"
---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+let teacherForms = document.getElementsByClassName("teacher-contract")
+for (let teacherForm of teacherForms) {
+  teacherForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    let price = teacherForm.price.value
+    let hours = teacherForm.hours.value
+    let total = price * hours
+
+    alert("O valor total do contrato é de R$ " + total)
+  })
+}
